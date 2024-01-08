0b5fadd65ee895ac6eccad8047b58b3e2ef74b16
abilova-madina madi <abilovamadii@gmail.com>
2024-01-08 21:51:29 +0000

    whatsapp,app.js

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 "use strict";
 $(document).ready(function () {
 
+    $("#me").click(function (e) {
+        e.preventDefault();
+        if(!$.trim($('inputme').val()) == "") {
+            
+        }
+    });
+
+
 });
